c8d93d501faf4a525ed220178b576f781eaaf903
Fidel Ngatse <fngatse@sessionm.com>
2021-11-05 00:13:14 +0100

    date change and typo fix

--- 02postman/testrail_upload.sh
+++ 02postman/testrail_upload.sh
@@ -4,7 +4,7 @@ TESTRAIL_DOMAIN=signosoft.testrail.io
 TESTRAIL_USERNAME=tom.signosoft@signosoft.com 
 TESTRAIL_APIKEY=ANLaCkgeIaDq9YStec2t-Xf/cbPT8EjZz8RFsEJWG 
 TESTRAIL_PROJECTID=3
-DATE=$( date '+%F_%H:%M:%S' )
+DATE=$( date '+%d-%m-%Y %H:%M' )
 TESTRAIL_TITLE="Signosoft API v1 Tests $DATE" 
 newman run SignosoftAPITests.postman_collection.json --environment TEST.postman_environment.json --reporters cli,testrail
  
