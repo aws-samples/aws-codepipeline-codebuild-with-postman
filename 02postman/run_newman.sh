32d8453279f9977719ae66e661642441e27b8172
Fidel Ngatse <fngatse@sessionm.com>
2021-11-04 22:54:31 +0100

    update variables

--- 02postman/run_newman.sh
+++ 02postman/run_newman.sh
@@ -4,7 +4,6 @@ TESTRAIL_DOMAIN=https://signosoft.testrail.io/
 TESTRAIL_USERNAME=tom.signosoft@signosoft.com 
 TESTRAIL_APIKEY=t8YzLgXHVMCu4IFHf7iM-of/edM3r47XOu6XyLzSB 
 TESTRAIL_PROJECTID=3
-TESTRAIL_RUNID=69 
 TESTRAIL_TITLE="Signosoft API v1 Tests" 
 newman run SignosoftAPITests.postman_collection.json --environment TEST.postman_environment.json --reporters cli,testrail
  
