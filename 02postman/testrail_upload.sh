f8da7df1c4967b303c520184849a522325abe719
Fidel Ngatse <fngatse@sessionm.com>
2021-11-05 00:24:29 +0100

    added matching title

--- 02postman/testrail_upload.sh
+++ 02postman/testrail_upload.sh
@@ -4,7 +4,7 @@ TESTRAIL_DOMAIN=signosoft.testrail.io
 TESTRAIL_USERNAME=tom.signosoft@signosoft.com 
 TESTRAIL_APIKEY=ANLaCkgeIaDq9YStec2t-Xf/cbPT8EjZz8RFsEJWG 
 TESTRAIL_PROJECTID=3
-DATE=$( date '+%d-%m-%Y %H:%M' )
-TESTRAIL_TITLE="Signosoft API v1 Tests $DATE" 
+DATE=$( date '+%d-%m-%Y %H:%M:%S' )
+TESTRAIL_TITLE="IntegrationTests[APIv1] Automated Test Run $DATE" 
 newman run SignosoftAPITests.postman_collection.json --environment TEST.postman_environment.json --reporters cli,testrail
  
