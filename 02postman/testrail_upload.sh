cdf528448ae2a0f3cc986890480c2a9b1629ae25
Fidel Ngatse <fngatse@sessionm.com>
2021-11-05 00:31:49 +0100

    typo

--- 02postman/testrail_upload.sh
+++ 02postman/testrail_upload.sh
@@ -5,6 +5,6 @@ TESTRAIL_USERNAME=tom.signosoft@signosoft.com
 TESTRAIL_APIKEY=ANLaCkgeIaDq9YStec2t-Xf/cbPT8EjZz8RFsEJWG 
 TESTRAIL_PROJECTID=3
 DATE=$( date '+%d-%m-%Y %H:%M:%S' )
-TESTRAIL_TITLE="IntegrationTests[APIv1] Automated Test Run $DATE" 
+TESTRAIL_TITLE="IntegrationTests[API] Automated Test Run $DATE" 
 newman run SignosoftAPITests.postman_collection.json --environment TEST.postman_environment.json --reporters cli,testrail
  
